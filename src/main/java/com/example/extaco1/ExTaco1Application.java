d52be9f8b1374c8a380e68af07d5f2113f65e7a5
Laukhin <i.laukhin@evotor.ru>
2023-01-27 10:17:02 +0300

    add get home page method

---
+++ src/main/java/com/example/extaco1/ExTaco1Application.java
@@ -0,0 +1,13 @@
+package com.example.extaco1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExTaco1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExTaco1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/extaco1/ExTaco1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.extaco1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExTaco1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
